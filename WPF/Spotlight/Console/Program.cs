faa9e6695e01e0246a8d4819a87aadac0d3799fa
yelkar-snehal <yelkarsnehal9@gmail.com>
2019-08-07 00:45:26 +0530

    first commit to wpf

---
+++ WPF/Spotlight/Console/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace access1
+{
+    class Program
+    {
+
+        public static List<string> SearchFile(string root)
+        {
+            var files = new List<string>();
+			
+			//add only files to the list by enumerating
+            foreach (var file in Directory.EnumerateFiles(root))
+            {
+                files.Add(file);
+            }
+            //enumaerate sub dir
+            foreach (var subDir in Directory.EnumerateDirectories(root))
+            {
+                try
+                {
+                	//addrange appends the whole collection to the list
+                	//recursive call to get files from sub dirs
+                    files.AddRange(SearchFile(subDir));
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                }
+            }
+
+            return files;
+        }
+
+        public static void Display( string drive, string fname)
+        {
+            Console.WriteLine("in drive: "+ drive);
+            //to avoid path err
+            StringBuilder Path = new StringBuilder(drive);
+            StringBuilder file = new StringBuilder();
+            file.Append(@"\");
+            file.Append(fname);
+            Path.Append(@"\");
+            try
+            {
+                var files = SearchFile(Path.ToString());
+
+                //check if entered file name exits in all the present files
+                foreach (string e in files)
+                {
+                    //Console.WriteLine(e);
+                    if (e.Contains(file.ToString()))
+                    {
+                        Console.WriteLine("file found " + e);
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                
+            }
+
+        }
+
+		//entry pt
+        static void Main(string[] args)
+        {
+        	//get ip
+            Console.WriteLine("enter file name");
+            string str = Console.ReadLine();
+            
+            /* allDrive - array of type driveinfo representing local drives on computer
+            *rturned by getdrives
+            */
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                //Console.WriteLine("Drive {0}", d.Name);
+                /*
+                * create a seprate thread for each drive present on the system
+                * use delegate for passing multiple params to the method run by the thread
+                * can use lambda in c# 3 () = > inside threadstart
+                */
+                Thread t = new Thread(new ThreadStart(delegate { Display(d.Name, str); }));
+                t.Start();
+                    
+            }
+        }
+    }
+}
+
---
+++ WPF/Spotlight/wpf/Spotlight/Spotlight/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+
+namespace Spotlight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        bool chk = true;
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            SearchResults.Visibility = Visibility.Visible;
+            SearchResults.IsEnabled = true;
+            chk = true;
+            search();
+            
+        }
+        
+
+        public void search()
+        {
+            if(chk)
+            {
+                DriveInfo[] allDrives = DriveInfo.GetDrives();
+                string str = fname.Text;
+
+                foreach (DriveInfo d in allDrives)
+                {
+                    //Console.WriteLine("Drive {0}", d.Name);
+
+                    Thread t = new Thread(new ThreadStart(delegate { Display(d.Name, str); }));
+                    t.Start();
+                    //SearchResults.AppendText(d.Name);
+                    //SearchResults.AppendText("\n");
+
+                }
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            chk = false;
+            SearchResults.IsReadOnly = true;
+        }
+
+        //////////////////////////////////////////////////////////
+        public void Display(string drive, string fname)
+        {
+            //Console.WriteLine("in drive: " + drive);
+
+            StringBuilder Path = new StringBuilder(drive);
+            StringBuilder file = new StringBuilder();
+            file.Append(@"\");
+            file.Append(fname);
+            Path.Append(@"\");
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+               // SearchResults.AppendText(Path.ToString());
+               // SearchResults.AppendText("\n");
+                try
+                {
+                    var files = SearchFile(Path.ToString());
+
+
+                    foreach (string e in files)
+                    {
+                        //Console.WriteLine(e);
+                        if (e.Contains(file.ToString()))
+                        {
+                            SearchResults.AppendText(e);
+                            SearchResults.AppendText("\n");
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    using (StreamWriter w = File.AppendText("Log.txt"))
+                       {
+                           w.WriteLine(e);
+                       }
+                }
+            }));
+
+        }
+        ///////////////////////////////////////////////////////////////////
+        public List<string> SearchFile(string root)
+        {
+            var files = new List<string>();
+
+            foreach (var file in Directory.EnumerateFiles(root))
+            {
+                files.Add(file);
+            }
+            foreach (var subDir in Directory.EnumerateDirectories(root))
+            {
+                try
+                {
+                    files.AddRange(SearchFile(subDir));
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    using (StreamWriter w = File.AppendText("Log.txt"))
+                     {
+                         w.WriteLine(ex);
+                     }
+
+                }
+            }
+
+            return files;
+        }
+
+    }
+
+}
